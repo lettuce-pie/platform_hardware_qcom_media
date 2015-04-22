e011dbf61de33c551ab57ef7b9aec960f0ef66bb
Steve Kondik <steve@cyngn.com>
2015-04-21 23:05:41 -0700

    vidc: Fix makefiles
    Change-Id: I0593222b3f487e80db1e4f2b5055a9ea5bccef60

--- Android.mk
+++ Android.mk
@@ -6,6 +6,8 @@ include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 
 ifneq ($(TARGET_SUPPORTS_WEARABLES),true)
+ifneq ($(filter msm8909 msm8916 msm8939,$(TARGET_BOARD_PLATFORM)),)
+
 include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
 include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 endif
